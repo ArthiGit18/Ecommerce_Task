5bb31641bcd8ea7d7a2f9c8d0b8b4e0fd51ffb8c
ArthiGit18 <arthiar180@gmail.com>
2025-07-03 15:28:18 +0530

    static cart with add button

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Navbar from "@/components/Navbar";
+import ProductCard from "@/components/ProductCard";
 
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <ProductCard />
     </>
   );
 }
